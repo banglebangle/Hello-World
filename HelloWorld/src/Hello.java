d934abffb89600a21c7b097483581cf4f0de1614
Zhang, Wenyu <WZhang2d6466@StateStreet.com>
2018-07-02 14:44:56 +0800

    TEST TEST TEST

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -4,6 +4,10 @@ public class Hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Win Two");
+		System.out.println("Win Two");
+		System.out.println("Win Two");
+		System.out.println("Win Two");
+		System.out.println("Win Two");
 	}
 
 }
