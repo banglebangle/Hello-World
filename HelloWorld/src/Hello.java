350a5fd12ac6ba3316e308989d1b05a4af5a0c87
Zhang, Wenyu <WZhang2d6466@StateStreet.com>
2018-07-02 14:26:57 +0800

    Different

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -6,6 +6,7 @@ public class Hello {
 		System.out.println("Hello World");
 		System.out.println("Hello World Again");
 		System.out.println("Hello World Again Again");
+		System.out.println("Win One");
 	}
 
 }
