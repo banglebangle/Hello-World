2fe6ce8d6bfa6844826d89cf7de957c7fd6e95ef
Zhang, Wenyu <WZhang2d6466@StateStreet.com>
2018-07-02 14:31:01 +0800

    Test Different

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -3,9 +3,6 @@ public class Hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World");
-		System.out.println("Hello World Again");
-		System.out.println("Hello World Again Again");
 		System.out.println("Win One");
 	}
 
